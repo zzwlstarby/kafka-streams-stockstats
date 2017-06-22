9a7d473cb2e6f7f1346aab76667b804d2737435c
001 <001@LAPTOP-JTOPE8V7>
2017-06-22 09:34:23 +0800

    增加getPrice方法

--- src/main/java/com/shapira/examples/streams/stockstats/model/Trade.java
+++ src/main/java/com/shapira/examples/streams/stockstats/model/Trade.java
@@ -16,6 +16,11 @@ public class Trade {
         this.price = price;
         this.size = size;
     }
+    
+    public double getPrice() {
+    	return price;
+    }
+    
 
     @Override
     public String toString() {
